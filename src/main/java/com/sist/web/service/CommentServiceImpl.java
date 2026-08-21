7afb8edde04cdd01ee1e0394caaa489e03e8e224
alpenglow93 <dktmzptm@gmail.com>
2026-08-21 14:17:13 +0900

    260821

--- src/main/java/com/sist/web/service/CommentServiceImpl.java
+++ src/main/java/com/sist/web/service/CommentServiceImpl.java
@@ -33,4 +33,16 @@ public class CommentServiceImpl implements CommentService
 		mapper.commentInsert(vo);
 	}
 
+	@Override
+	public void commentDelete(int no) {
+		// TODO Auto-generated method stub
+		mapper.commentDelete(no);
+	}
+
+	@Override
+	public void commentUpdate(CommentVO vo) {
+		// TODO Auto-generated method stub
+		mapper.commentUpdate(vo);
+	}
+
 }
